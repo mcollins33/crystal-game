7178feb2a54b83703164f88020207019b988e53a
mcollins33 <collins.miki@gmail.com>
2017-11-22 18:29:09 -0500

    created functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,84 @@
 $(document).ready(function() {
 
-
     var wins = 0;
     var losses = 0;
     var randomNumber;
     var score = 0;
     var start = true;
+    var j1;
+    var j2;
+    var j3;
+    var j4;
 
-    randomNumber = Math.floor(Math.random() * (121 - 19) + 19);
-    $("#match").text(randomNumber);
-    console.log(randomNumber);
-
-    var jewel1 = Math.floor(Math.random() * 12) + 1;
-    console.log(jewel1);
-
-    var jewel2 = Math.floor(Math.random() * 12) + 1;
-    console.log(jewel2);
-
-    var jewel3 = Math.floor(Math.random() * 12) + 1;
-    console.log(jewel3);
-
-    var jewel4 = Math.floor(Math.random() * 12) + 1;
-    console.log(jewel4);
-
-    $("#jewel1").on("click", function() {
-        score = score + jewel1;
+    function random() {
         $("#score").text(score);
-    });
 
-    $("#jewel2").on("click", function() {
-        score += jewel2;
-        $("#score").text(score);
-    });
+        randomNumber = Math.floor(Math.random() * (121 - 19) + 19);
+        $("#match").text(randomNumber);
 
-    $("#jewel3").on("click", function() {
-        score += jewel3;
-        $("#score").text(score);
-    });
-
-    $("#jewel4").on("click", function() {
-        score += jewel4;
-        $("#score").text(score);
-    });
+        j1 = Math.floor(Math.random() * 12) + 1;
+        j2 = Math.floor(Math.random() * 12) + 1;
+        j3 = Math.floor(Math.random() * 12) + 1;
+        j4 = Math.floor(Math.random() * 12) + 1;
+        start = false;
+    };
 
 
-    if (score === randomNumber) {
-        wins++;
-        console.log(wins);
+    function checkForWin() {
+        if (score === randomNumber) {
+            wins++;
+            $("#wins").text(wins);
+            $("#score").text(score);
+            reset();
+
+        } else if (score > randomNumber) {
+            losses++;
+            $("#losses").text(losses);
+            $("#score").text(score);
+            reset();
+        }
+    };
+
+    function reset() {
         start = true;
-    } else if (score > randomNumber) {
-        losses++;
-        console.log(losses);
-        start = true;
+        randomNumber;
+        score = 0;
+        $("#match").text(randomNumber);
+        $("#score").text(score);
+        random();
+
+    };
+
+    if (start === true) {
+        random();
     }
 
+    if (start === false && score < randomNumber) {
 
+        $("#jewel1").on("click", function() {
+            score += j1;
+            $("#score").text(score);
+            checkForWin();
+        });
 
+        $("#jewel2").on("click", function() {
+            score += j2;
+            $("#score").text(score);
+            checkForWin();
+        });
+
+        $("#jewel3").on("click", function() {
+            score += j3;
+            $("#score").text(score);
+            checkForWin();
+        });
+
+        $("#jewel4").on("click", function() {
+            score += j4;
+            $("#score").text(score);
+            checkForWin();
+        });
+    }
 
 
 
